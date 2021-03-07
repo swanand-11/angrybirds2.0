c0ad588ff430e78494f8b2636ba03a370a144c56
swanand-11 <swanand.sutar@icloud.com.com>
2021-03-07 09:51:51 +0530

    we made contrained bodies

---
+++ chain.js
@@ -0,0 +1,20 @@
+class Chain {
+   constructor(bodyA,bodyB) {
+    var options = {
+        bodyA:bird.body,
+        bodyB:clog.body,
+        length:10,
+        stiffness:0.04,
+       }
+       this.chain = Constraint.create(options)
+       World.add(world,this.chain)
+   }                 
+        display()  {
+            var pointA = this.chain.bodyA.position
+            var pointB = this.chain.bodyB.position
+            strokeWeight(7)
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+    
+        }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var clog,chain; 
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +37,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    clog = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,clog.body)
+
+
+   
+
+    
+
 
 }
 
@@ -43,6 +53,8 @@ function draw(){
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
+
+    
     console.log(box2.body.angle);
     box1.display();
     box2.display();
@@ -61,4 +73,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    clog.display();
+    chain.display();
 }
